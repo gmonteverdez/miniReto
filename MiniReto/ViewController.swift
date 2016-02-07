946c0d1fb47e9dd011c0e010474a20793ff0b134
Gerardo Monteverde <jerry.monteverdez@gmail.com>
2016-02-07 04:59:46 -0700

    Mini Reto Gerardo

--- MiniReto/ViewController.swift
+++ MiniReto/ViewController.swift
@@ -8,10 +8,37 @@
 
 import UIKit
 
+let todosLosNumeros = 1...100
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        for numero in todosLosNumeros{
+            
+            
+            if numero % 5 == 0{
+                print("\(numero) Bingo!!")
+            }
+            
+            if numero % 2 != 0{
+                print("\(numero) Impar")
+                
+            }else{
+                print("\(numero) Par!")
+            }
+            
+            if numero >= 30 && numero <= 40{
+                print("\(numero) VIVA SWIFT!")
+            }
+            
+        }
+        
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
